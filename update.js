16d2d3a3d23f5532ce55ba4b2af4c7b397aa7b02
Nick0603 <b10303008@gmail.com>
2017-09-23 02:41:59 +0800

    add update.js

---
+++ update.js
@@ -0,0 +1,9 @@
+swal({
+    title: '哈囉，工具目前需要更新!',
+    text: '請問你要現在更新嗎?',
+    imageUrl: 'update.png',
+    imageWidth: 640,
+    imageHeight: 480,
+    imageAlt: 'Custom image',
+    animation: false
+  })
